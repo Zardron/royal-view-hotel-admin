5261a8ea47c531c7726d8a5c60df7f020a898ddd
Zardron <zardron@clockworkdelivery.com>
2023-09-24 16:06:58 +0400

    adding minor changes

--- src/scenes/room-details/index.js
+++ src/scenes/room-details/index.js
@@ -38,8 +38,6 @@ const RoomDetails = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
 
-  console.log(loading);
-
   useEffect(() => {
     axios
       .get(`https://rvh-backend.vercel.app/api/room/room-details/${id}`)
